7ec9a25063b76cddbc6ea829e9abc8b45286f1bf
Kanjanass3 <s6506021632014@email.kmutnb.ac.th>
2025-03-29 23:31:36 +0700

    main menu datatype variable operator Control statement,function

--- app/src/main/java/com/example/myapplication/FunctionsActivity.java
+++ app/src/main/java/com/example/myapplication/FunctionsActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class FunctionsActivity extends AppCompatActivity {
-
+public class FunctionsActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnBackFunction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_functions);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnBackFunction = findViewById(R.id.btnBackFunction);
+        btnBackFunction.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.btnBackFunction) {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }
     }
 }
